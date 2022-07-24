dbb1cc0fb97f014183bc341e97f701e895424c1c
brycepaquette <bp343475@gmail.com>
2022-07-24 18:03:05 -0400

    HW02 Completed

---
+++ module-3/HW02/Calculator.java
@@ -0,0 +1,124 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        // Instantiate Scanner Object
+        Scanner sc = new Scanner(System.in);
+
+        // My added code: Array of operations, error string
+        String[] opArr = {"add", "subtract", "multiply", "divide", "aplhabetize"};
+        String errorMsg = "Invalid input entered. Terminating...";
+        int intOne = 0;
+        int intTwo;
+        double dblOne = 0;
+        double dblTwo;
+        int resultInt;
+        double resultDbl;
+
+        
+        // Prompt the user to enter an operation. This operation must be processed as case-insensitive
+        System.out.println("List of Operations: " + String.join(" ", opArr));
+        
+        System.out.print("Enter an operation: ");
+        String op = sc.nextLine().toLowerCase();
+        switch(op) {
+            case "add":
+                // If the user is performing an add/subtract operation, prompt the user to enter two integers.
+                System.out.print("Enter two integers: ");
+
+                if (sc.hasNextInt())
+                    intOne = sc.nextInt();
+                if (sc.hasNextInt()) {
+                    intTwo = sc.nextInt();
+                    resultInt = intOne + intTwo;
+                    System.out.println("Answer: " + resultInt);
+                }    
+                else {
+                    System.out.println(errorMsg);
+                    System.exit(1);
+                }
+                break;
+            case "subtract":
+                // If the user is performing an add/subtract operation, prompt the user to enter two integers.
+                System.out.print("Enter two integers: ");
+
+                if (sc.hasNextInt())
+                    intOne = sc.nextInt();
+                if (sc.hasNextInt()) {
+                    intTwo = sc.nextInt();
+                    resultInt = intOne - intTwo;
+                    System.out.println("Answer: " + resultInt);
+                }
+                else {
+                    System.out.println(errorMsg);
+                    System.exit(1);
+                }
+                break;
+            case "multiply":
+                // If the user is performing a multiply/divide operation, prompt the user to enter two doubles.
+                System.out.print("Enter two doubles: ");
+
+                if (sc.hasNextDouble())
+                    dblOne = sc.nextDouble();
+                if (sc.hasNextDouble()) {
+                    dblTwo = sc.nextDouble();
+                    resultDbl = dblOne * dblTwo;
+                    System.out.printf("Answer: %.2f", resultDbl);
+                }
+                else {
+                    System.out.println(errorMsg);
+                    System.exit(1);
+                }
+                break;
+            case "divide":
+                // If the user is performing a multiply/divide operation, prompt the user to enter two doubles.
+                System.out.print("Enter two doubles: ");
+
+                if (sc.hasNextDouble())
+                    dblOne = sc.nextDouble();
+                if (sc.hasNextDouble()) {
+                    dblTwo = sc.nextDouble();
+                    if (dblTwo != 0.0) {
+                        resultDbl = dblOne / dblTwo;
+                        System.out.printf("Answer: %.2f", resultDbl);
+                    }
+                    else {
+                        System.out.println(errorMsg);
+                        System.exit(1);
+                    } 
+                }
+                else {
+                    System.out.println(errorMsg);
+                    System.exit(1);
+                }
+                break;
+            case "alphabetize":
+                // If the user is performing an alphabetize operation, prompt the user to enter two words.
+                System.out.print("Enter two words: ");
+                String input = sc.nextLine();
+                String[] words = input.split(" ");
+                if ((words.length == 2) && (input.matches("[a-zA-Z\\s\\n]+"))) {
+                    int compareWords = words[0].compareTo(words[1]);
+                    if ((compareWords == 0) || (words[0].equalsIgnoreCase(words[1]))) {
+                        System.out.println("Answer: Chicken or Egg.");
+                    }
+                    else if (compareWords > 0) {
+                        System.out.println("Answer: " + words[0] + " comes before " + words[1] + " alphabetically.");
+                    }
+                    else {
+                        System.out.println("Answer: " + words[1] + " comes before " + words[0] + " alphabetically.");
+                    }
+                }
+                else {
+                    System.out.println(errorMsg);
+                    System.exit(1);
+                }
+                break;
+            default:
+                // If the user enters an invalid operation, the program should print the following error message and terminate gracefully. "Invalid input entered. Terminating..."
+                System.out.println(errorMsg);
+                System.exit(1);
+        } 
+        System.exit(0);
+    }
+}
