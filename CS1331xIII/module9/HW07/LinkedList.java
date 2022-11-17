30ee4177533bf2c4da6a10e6ddc00aa4dfc7bf79
Bryce Paquette <49108569+brycepaquette@users.noreply.github.com>
2022-11-17 01:52:25 +0000

    added to LinkedList

--- CS1331xIII/module9/HW07/LinkedList.java
+++ CS1331xIII/module9/HW07/LinkedList.java
@@ -1,3 +1,29 @@
-public class LinkedList {
+public class LinkedList<T> implements List<T> {
     
+    Node<T> head;
+    Node<T> tail;
+    int size;
+
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public Node<T> getHead() {
+        return this.head;
+    }
+
+    public Node<T> getTail() {
+        return this.tail;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        System.out.println(list.getHead());
+        list.getTail();
+
+    }
+
+
 }
